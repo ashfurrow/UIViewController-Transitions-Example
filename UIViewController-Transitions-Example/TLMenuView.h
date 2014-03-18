496708a83376e0fc48710ce9f44ee745975d9c92
Enrico Angelini <enrico@spirales.it>
2014-03-18 22:21:26 +0100

    Root view subclassed to override setFrame

---
+++ UIViewController-Transitions-Example/TLMenuView.h
@@ -0,0 +1,13 @@
+//
+//  TLMenuView.h
+//  UIViewController-Transitions-Example
+//
+//  Created by Enrico Angelini on 18/03/14.
+//  Copyright (c) 2014 Teehan+Lax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLMenuView : UIView
+
+@end
---
+++ UIViewController-Transitions-Example/TLMenuView.m
@@ -0,0 +1,31 @@
+//
+//  TLMenuView.m
+//  UIViewController-Transitions-Example
+//
+//  Created by Enrico Angelini on 18/03/14.
+//  Copyright (c) 2014 Teehan+Lax. All rights reserved.
+//
+
+#import "TLMenuView.h"
+
+@implementation TLMenuView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
